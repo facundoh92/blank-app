f7e5681f196b4abc2bec64b1369a497f9c9084d2
jorge <72173605+facundoh92@users.noreply.github.com>
2024-11-13 01:16:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
 #selected_nationality = st.selectbox("Select Nationality", "Select Position")
 option = st.selectbox(
     "Elija una opcion:",
